006f6554d2ec6abae6769b8deb6040358f3a6e23
mushther <mushther2121999@gmai.com>
2022-08-28 18:46:50 +0530

    added new file

--- login.js
+++ login.js
@@ -7,22 +7,30 @@ function myfun1(event){
 
     let email=document.querySelector("#email").value;
     let password=document.querySelector("#password").value;
-    
-    loginData.forEach(function(ele){
-        if(email==ele.email){
-            if(password==ele.password){
-                window.location.href="home_page.html"
-            }else{
-                document.querySelector("p").innerText="Password Wrong";
+    if(email=="" && password==""){
+        document.querySelector("p").innerText="Enter email Id & password";
+        document.querySelector("p").style.color="red"
+    }else if(password==""){
+        document.querySelector("p").innerText="Enter password";
+        document.querySelector("p").style.color="red"
+    }else if(email=="" ){
+        document.querySelector("p").innerText="Enter email Id";
+        document.querySelector("p").style.color="red"
+    }else{
+        loginData.forEach(function(ele){
+            if(email==ele.email && password==ele.password){
+                    window.location.href="home_page.html"
+            }else if(email !==ele.email){
+                document.querySelector("p").innerText="Email Wrong";
+                document.querySelector("p").style.color="red"
+            }else if(password !== ele.password){
+                    document.querySelector("p").innerText="Password Wrong";
+                    document.querySelector("p").style.color="red"
+            }else {
+                document.querySelector("p").innerText="Email & Password Wrong";
                 document.querySelector("p").style.color="red"
             }
-            //console.log("correct email")
-        }else if(email==ele.email){
-            document.querySelector("p").innerText="Email Wrong";
-            document.querySelector("p").style.color="red"
-        }else{
-            document.querySelector("p").innerText="Email & Password Wrong";
-            document.querySelector("p").style.color="red"
-        }
-    }) 
+        }) 
+
+    }
 }
